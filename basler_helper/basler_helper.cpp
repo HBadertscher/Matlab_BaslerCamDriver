6a762416511d71c80471402d6a22fa14d93cfeaa
Hannes Badertscher <hbaderts@hsr.ch>
2015-04-21 13:23:56 +0200

    Internal helper functions to get/set camera options

---
+++ basler_helper/basler_helper.cpp
@@ -0,0 +1,148 @@
+// basler_helper.cpp - A collection of helper functions for Basler cameras
+// 21.04.2015 / HB
+
+
+#include "basler_helper.h"
+#include <mex.h>
+#include <pylon/PylonIncludes.h>
+
+
+namespace BaslerHelper {
+    //---------------------------------------------------------------------
+    // Set float parameter
+    void set_parameter( Pylon::CInstantCamera* p_camera, const char* s_param_name, const double* new_value, const bool b_verbose)    {
+        GenApi::CPointer<GenApi::IFloat> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        if (IsWritable(p_parameter))
+		{
+			p_parameter->SetValue(*new_value);
+        }
+        else
+        {
+            throw( RUNTIME_EXCEPTION("Parameter not writable. Maybe wrong datatype?"));
+        }
+        if(b_verbose)
+        {
+            printf("Set Parameter \"%s\" to %f\n",s_param_name, *new_value);
+        }
+    }
+    
+    //---------------------------------------------------------------------
+    // Set int parameter
+	void set_parameter(Pylon::CInstantCamera* p_camera, const char* s_param_name, const int* new_value, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IInteger> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        if (IsWritable(p_parameter))
+		{
+			p_parameter->SetValue(*new_value);
+        }
+        else
+        {
+            throw( RUNTIME_EXCEPTION("Parameter not writable. Maybe wrong datatype?"));
+        }
+        if(b_verbose)
+        {
+            printf("Set Parameter \"%s\" to %d\n",s_param_name, *new_value);
+        }
+    }
+    
+    //---------------------------------------------------------------------
+    // Set string parameter
+	void set_parameter(Pylon::CInstantCamera* p_camera, const char* s_param_name, const char* new_value, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IEnumeration> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        if (IsWritable(p_parameter))
+		{
+			p_parameter->FromString(new_value);
+        }
+        else
+        {
+            throw( RUNTIME_EXCEPTION("Parameter not writable. Maybe wrong datatype?"));
+        }
+        if(b_verbose)
+        {
+            printf("Set Parameter \"%s\" to %s\n",s_param_name, new_value);
+        }
+    }
+    
+    //---------------------------------------------------------------------
+    // Set bool parameter
+	void set_parameter(Pylon::CInstantCamera* p_camera, const char* s_param_name, const bool* new_value, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IBoolean> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        if (IsWritable(p_parameter))
+		{
+			p_parameter->SetValue(*new_value);
+        }
+        else
+        {
+            throw( RUNTIME_EXCEPTION("Parameter not writable. Maybe wrong datatype?"));
+        }
+        if(b_verbose)
+        {
+            printf("Set Parameter \"%s\" to %d\n",s_param_name, *new_value);
+        }
+    }
+    
+    //---------------------------------------------------------------------
+    // Get float parameter
+    double get_float( Pylon::CInstantCamera* p_camera, const char* s_param_name, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IFloat> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        
+        if(b_verbose)
+        {
+            std::string s_result = p_parameter->ToString();
+            printf("Read Parameter \"%s\": %s\n",s_param_name, s_result);
+        }
+        
+        return p_parameter->GetValue();
+    }
+    
+    //---------------------------------------------------------------------
+    // Get int parameter
+    int get_int( Pylon::CInstantCamera* p_camera, const char* s_param_name, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IInteger> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        
+        if(b_verbose)
+        {
+            std::string s_result = p_parameter->ToString();
+            printf("Read Parameter \"%s\": %s\n",s_param_name, s_result);
+        }
+        
+        return p_parameter->GetValue();
+    } 
+
+    //---------------------------------------------------------------------
+    // Get string parameter
+    std::string get_string( Pylon::CInstantCamera* p_camera, const char* s_param_name, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IEnumeration> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        
+        if(b_verbose)
+        {
+            std::string s_result = p_parameter->ToString();
+            printf("Read Parameter \"%s\": %s\n",s_param_name, s_result);
+        }
+        
+        return p_parameter->ToString();
+    }
+    
+    //---------------------------------------------------------------------
+    // Get bool parameter
+    bool get_bool( Pylon::CInstantCamera* p_camera, const char* s_param_name, const bool b_verbose)
+    {
+        GenApi::CPointer<GenApi::IBoolean> p_parameter = p_camera->GetNodeMap().GetNode(s_param_name);
+        
+        if(b_verbose)
+        {
+            std::string s_result = p_parameter->ToString();
+            printf("Read Parameter \"%s\": %s\n",s_param_name, s_result);
+        }
+        
+        return p_parameter->GetValue();
+    } 
+    
+    
+
+}
---
+++ basler_helper/basler_helper.h
@@ -0,0 +1,59 @@
+// basler_helper.h - A collection of helper functions for Basler cameras
+// 21.04.2015 / HB
+
+#ifndef __BASLERHELPER_H_INCLUDED__
+#define __BASLERHELPER_H_INCLUDED__
+
+#include <string>
+#include <pylon/PylonIncludes.h>
+
+
+namespace BaslerHelper {
+
+    // Set float parameter
+    void set_parameter( Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const double* new_value, 
+                        const bool b_verbose);
+    
+    // Set int parameter
+    void set_parameter( Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const int* new_value, 
+                        const bool b_verbose);
+    
+    // Set string parameter
+    void set_parameter( Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const char* new_value, 
+                        const bool b_verbose);
+
+    // Set bool parameter
+    void set_parameter( Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const bool* new_value, 
+                        const bool b_verbose);
+
+    // Get float parameter
+    double get_float(	Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const bool b_verbose);
+
+    // Get int parameter
+    int get_int(        Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const bool b_verbose);
+    
+    // Get bool parameter
+    bool get_bool(      Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const bool b_verbose);
+
+    // Get string parameter
+    std::string get_string( Pylon::CInstantCamera* p_camera, 
+                        const char* s_param_name, 
+                        const bool b_verbose);
+    
+}
+
+#endif 
