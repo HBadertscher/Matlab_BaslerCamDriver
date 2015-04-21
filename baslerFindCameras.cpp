c407a85c3ccf5c1c1e77e96e55151a6236fe6f9f
Hannes Badertscher <hbaderts@hsr.ch>
2015-04-21 09:50:36 +0200

    Created baslerFindCameras

---
+++ baslerFindCameras.cpp
@@ -0,0 +1,48 @@
+// baslerFindCameras.cpp - Find all connected Basler cameras
+// see baslerFindCameras.m for help
+
+#include <pylon/PylonIncludes.h>
+
+#include <matrix.h>
+#include <mex.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{       
+    
+	// Initiatlize Pylon
+	Pylon::PylonAutoInitTerm auto_init_term;
+
+    try
+    {
+        // Get the transport layer factory.
+        Pylon::CTlFactory& tlFactory = Pylon::CTlFactory::GetInstance();
+        
+        // Get all attached devices
+        Pylon::DeviceInfoList_t devices;
+        tlFactory.EnumerateDevices(devices);
+
+        
+        // Create an array of instant cameras for the found devices
+        Pylon::CInstantCameraArray cameras(devices.size());
+
+        // Init output
+        plhs[0] = mxCreateCellMatrix(cameras.GetSize(),2);
+        
+        // Find all names
+        for(size_t i = 0; i < cameras.GetSize(); ++i)
+        {
+            cameras[i].Attach(tlFactory.CreateDevice(devices[i]));
+            mxSetCell(plhs[0],i+cameras.GetSize(),mxCreateString(cameras[i].GetDeviceInfo().GetModelName()));
+            mxSetCell(plhs[0],i,mxCreateDoubleScalar(i));
+        }
+        
+    }
+	catch (GenICam::GenericException &e)
+	{
+		// Error handling.
+        mexErrMsgIdAndTxt("aca_driver:Error:CameraError",e.GetDescription());
+	}
+    
+	return;
+}
+
---
+++ baslerFindCameras.m
@@ -0,0 +1,8 @@
+% baslerFindCameras.m - Find all connected Basler cameras
+%
+%  Returns a cell array containing the index (first column) and name 
+%  (second column) of all found Basler cameras. 
+%
+%  Usage:
+%  available_cams = find_cameras();
+%
